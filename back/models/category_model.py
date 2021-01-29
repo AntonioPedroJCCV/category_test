d4ada418fbaaa2e341f11329295245059c7d4cb3
marcosdcl <marcosdouglasclima@gmail.com>
2021-01-29 18:29:09 -0300

    Implement util validator in category model

--- back/models/category_model.py
+++ back/models/category_model.py
@@ -2,6 +2,12 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import validates
 from back.models.base_model import BaseModel
 
+from back.utils.validators import (
+    validate_len, 
+    validate_not_empty,
+    validate_type
+)
+
 
 class Category(BaseModel):
     __tablename__ = 'category'
@@ -12,22 +18,13 @@ class Category(BaseModel):
         self.name = name
         self.description = description
 
-
     @validates('name')
     def validate_name(self, key, name):
-        if not isinstance(name, str):
-            raise TypeError("The name must be a string")
-        if not name.strip():
-            raise ValueError("The name can't be empty")
-        if len(name) > 100:
-            raise ValueError("The name can't exceed 100 characters")
-        return name
-
+        name = validate_type(key, name, str)
+        name = validate_not_empty(key, name)
+        return validate_len(key, name, 100)
 
     @validates('description')
     def validate_description(self, key, description):
-        if not isinstance(description, str):
-            raise TypeError("The description must be a string!")
-        if len(description) > 255:
-            raise ValueError("The description can't exceed 255 or less characters!")
-        return description
+        description = validate_type(key, description, str)
+        return validate_len(key, description, 255)
