6077946281d332ce8a2c4d14a22991f813078433
marcosdcl <marcosdouglasclima@gmail.com>
2021-01-29 17:07:32 -0300

    Update test with pytest raises

--- back/tests/models_tests/category_model_test.py
+++ back/tests/models_tests/category_model_test.py
@@ -1,40 +1,30 @@
 import sys
 sys.path.append('.')
 
-
+import pytest
 from back.models.category_model import Category
 
 
-def test_empty_name():
-    try:
-        category_test = Category('', 'Description test')
-    except Exception as e:
-        assert isinstance(e, ValueError)
-
-
 def test_type_name():
-    try:
+    with pytest.raises(TypeError):
         category_test = Category(25.0, 'Description test')
-    except Exception as e:
-        assert isinstance(e, TypeError)
+
+
+def test_empty_name():
+    with pytest.raises(ValueError):
+        category_test = Category('', 'Description test')
 
 
 def test_len_name():
-    try:
-        category_test = Category('Name test' * 500, 'Description test')
-    except Exception as e:
-        assert isinstance(e, ValueError)
+    with pytest.raises(ValueError):
+        category_test = Category('i'*101, 'Description test')
 
 
 def test_type_description():
-    try:
+    with pytest.raises(TypeError):
         category_test = Category('Name test', 25.0)
-    except Exception as e:
-        assert isinstance(e, TypeError)
 
 
 def test_len_description():
-    try:
+    with pytest.raises(ValueError):
         category_test = Category('Name test', 'Description test' * 500)
-    except Exception as e:
-        assert isinstance(e, ValueError)
